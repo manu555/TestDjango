e4b1c83c8a7a6b81e4cc0d4691b0608e6db829f4
Manuel <mperezg@ategix.com>
2016-05-21 15:07:30 -0400

    Added CSS, project creation, views, etc. - completed tutorial

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.project_list, name='project_list')
+    url(r'^$', views.project_list, name='project_list'),
+    url(r'^handler/(?P<pk>\d+)/$', views.handler_projects, name='handler_projects'),
+    url(r'^project/new/$', views.project_new, name='project_new')
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+from django.contrib.auth.models import User
+from django.utils import timezone
+from .forms import ProjectForm
 
 # Create your views here.
 
 def project_list(request):
-    return render(request, 'testapp/project_list.html', {})
+    projects = Project.objects.filter(last_updated_date__lte=timezone.now()).order_by('last_updated_date')
+    return render(request, 'testapp/project_list.html', {'projects' : projects})
+
+def handler_projects(request, pk):
+    handler = get_object_or_404(User, pk=pk)
+    show_projects = Project.objects.filter(Handler=handler)
+    return render(request, 'testapp/handler_projects.html', {"handler" : handler, "show_projects" : show_projects})
+
+def project_new(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.Handler = request.user
+            project.created_date = timezone.now()
+            project.save()
+    else:
+        form = ProjectForm()
+    return render(request, 'testapp/post_edit.html', {'form' : form})
